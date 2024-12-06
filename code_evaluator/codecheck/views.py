3febf7c68ec16dd0fe0db88117246941c6c715b8
Shekinah Florance <84413589+flora-shek@users.noreply.github.com>
2024-12-06 07:37:57 +0000

    layout

--- code_evaluator/codecheck/views.py
+++ code_evaluator/codecheck/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+    
